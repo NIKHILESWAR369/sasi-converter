1fa9c1c581ec632c7bea11ef1be00f652b5adaab
NIKHILESWAR369 <nikhileswar.relangi@sasi.ac.in>
2025-01-21 15:35:02 +0530

    susscsful

--- convert.test.js
+++ convert.test.js
@@ -6,5 +6,8 @@ test('converts 0 INR to USD', () => {     expect(convert(0)).toBe('0.00');
 test('handles negative values', () => {     expect(convert(-100)).toBe('-1.20'); 
 }); 
 test('handles non-numerical values', () => { 
-    expect(()=> convert('ten')).toThrow("Invalid amount"); }); 
+    expect(()=> convert('ten')).toThrow("Invalid amount"); });
+test('handles non-numerical values', () => {
+    expect(()=> convert('10n')).toThrow("Invalid amount"); });
+
 
